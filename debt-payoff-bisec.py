1ec641db8ab4923ff903a4a7fc205775cefb777d
alexwbishop <alex.w.bishop@gmail.com>
2015-09-10 09:41:58 -1000

    Update debt-payoff-bisec.py

--- debt-payoff-bisec.py
+++ debt-payoff-bisec.py
@@ -2,10 +2,10 @@
 # alex.w.bishop@gmail.com
 # computes a fixed monthly payment to pay off an interest-bearing debt within x months
 # uses bisection search
-balance = float(raw_input("balance = ")) # current balance at beginning of year
+balance = float(raw_input("balance = ")) # starting balance to pay off
 annualInterestRate = float(raw_input("annual interest rate = ")) # 0 < .2float < 1
 period = int(raw_input("months to pay off by = ")) # int >= 1
-precision = float(raw_input("precision of monthly payment = ")) # float
+precision = float(raw_input("monthly payment to be a multiple of = ")) # int or float
 monthlyInterestRate = annualInterestRate/12.0
 
 def endBalance(monthlyPmt, balance):
@@ -24,7 +24,7 @@ def monthlyPmtPayoff(balance, annualInterestRate, period, precision):
     balance: .2float, balance to pay off
     annualInterestRate: 0 < .2float < 1
     period: int (# of months to reduce balance to 0)
-    precision = .2float, precision of monthlyPmt result'''
+    precision = int or .2float, precision of monthlyPmt result'''
     monthlyPmtMin = balance/period # LOWER BOUND OF SEARCH
     monthlyPmtMax = (balance * (1 + monthlyInterestRate)**period) / period # UPPER BOUND OF SEARCH
     monthlyPmt = (monthlyPmtMax + monthlyPmtMin)/2.0  # SET MIDPOINT
